11bab62b9f4776e1f49bd21bc90845ca7d704341
ivnikonHA <ivnikon@mail.ru>
2023-12-07 17:02:42 +0300

    Исправил ошибку автопроверки

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import TripEventsPresenter from './presenter/trip-events-presenter.js';
 import { render } from './render.js';
 import FilterView from './view/filter-view.js';
-import SortView from './view/sort-view.js';
 
 const siteFiltersElement = document.querySelector('.trip-controls__filters');
 const siteMainElement = document.querySelector('.trip-events');
