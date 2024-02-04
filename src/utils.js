fac4b1b6b08fbcae36166d5b19f387120062d10e
ivnikonHA <ivnikon@mail.ru>
2024-02-04 17:31:55 +0300

    Почистил utils

--- src/utils.js
+++ src/utils.js
@@ -31,23 +31,6 @@ function getDuration(startTime, endTime) {
   return returnString;
 }
 
-function getRandomArrayElement(items) {
-  return items[Math.floor(Math.random() * items.length)];
-}
-
-function getRandomNumber(from, to) {
-  const lower = Math.ceil(Math.min(Math.abs(from), Math.abs(to)));
-  const upper = Math.floor(Math.max(Math.abs(to), Math.abs(from)));
-  return Math.round(Math.random() * (upper - lower) + lower);
-}
-
-function incrementCounter(startFrom) {
-  let counter = startFrom;
-  return function () {
-    return counter++;
-  };
-}
-
 function updateItem(items, update) {
   return items.map((item) => item.id === update.id ? update : item);
 }
@@ -81,13 +64,10 @@ const checkPriceIsNumeric = (price) => /^\d+$/.test(+price);
 const capitalize = (str) => str[0].toUpperCase() + str.slice(1);
 
 export {
-  getRandomArrayElement,
-  getRandomNumber,
   humanizeEventDate,
   createDateTimeString,
   createTimeString,
   getDuration,
-  incrementCounter,
   updateItem,
   sortDayAscending,
   sortTimeAscending,
